ce7bd995f6a0bca7284d073ee867a55282ee2891
Ethan Lossner <vilespringethan@gmail.com>
2021-03-21 19:03:49 -0400

    Actual veterancy object

---
+++ Assets/Scripts/UI/Veterancy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chess
+{
+    namespace Piece
+    {
+        public class Veterancy
+        {
+            public PieceType pieceType;
+            public uint kills = 0;
+            public uint survival = 0;
+
+            public Veterancy(GamePieceBase piece)
+            {
+                pieceType = piece.type;
+            }
+        }
+    }
+}
